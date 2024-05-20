52e6f65d6c1b5845eab5992f7f8b4640eca469b9
Mallika_WSL <mallike.wy@gmail.com>
2024-05-20 15:55:09 +0800

    修改recovery_manager相关代码

--- src/include/recovery/log_rec.h
+++ src/include/recovery/log_rec.h
@@ -2,7 +2,7 @@
 #define MINISQL_LOG_REC_H
 
 #include <unordered_map>
-#include <utility>
+#include <utility> //工具类:提供了一组用于处理各种任务的实用模板类和函数
 
 #include "common/config.h"
 #include "common/rowid.h"
@@ -10,12 +10,12 @@
 
 enum class LogRecType {
   kInvalid,
-  kInsert,
-  kDelete,
-  kUpdate,
-  kBegin,
-  kCommit,
-  kAbort,
+  kInsert, // 数据的插入操作，表示某条记录已被插入数据库
+  kDelete, // 数据的删除操作，表示某条记录已被从数据库中删除
+  kUpdate, // 数据的更新操作，表示数据库中某条记录已被修改
+  kBegin,  // 事务开始，表示一个新的数据库事务已开始。
+  kCommit, // 事务提交，表示事务已成功完成，并且已经被永久写入数据库。
+  kAbort,  // 事务终止，表示一个数据库事务由于某种原因无法完成，所进行的任何操作都需要回滚到事务开始之前的状态。
 };
 
 // used for testing only
@@ -28,10 +28,10 @@ struct LogRec {
   LogRec(LogRecType type, lsn_t lsn, txn_id_t txn_id, lsn_t prev_lsn)
       : type_(type), lsn_(lsn), txn_id_(txn_id), prev_lsn_(prev_lsn) {}
 
-  LogRecType type_{LogRecType::kInvalid};
-  lsn_t lsn_{INVALID_LSN};
-  txn_id_t txn_id_{INVALID_TXN_ID};
-  lsn_t prev_lsn_{INVALID_LSN};
+  LogRecType type_{LogRecType::kInvalid}; // 记录日志项类型
+  lsn_t lsn_{INVALID_LSN}; // 日志序列号(Log Sequence Number),每个操作的唯一标识符
+  txn_id_t txn_id_{INVALID_TXN_ID}; // 事务的唯一标识符
+  lsn_t prev_lsn_{INVALID_LSN}; // 该日志上一次操作的序列号
   /* used for insert only */
   KeyType ins_key_{};
   ValType ins_val_{};
@@ -64,8 +64,11 @@ struct LogRec {
 std::unordered_map<txn_id_t, lsn_t> LogRec::prev_lsn_map_ = {};
 lsn_t LogRec::next_lsn_ = 0;
 
+// 智能指针,易于管理,避免内存相关的错误,自动释放内存
 typedef std::shared_ptr<LogRec> LogRecPtr;
 
+// 每一种日志的书写,都是新建一个LogRec对象,根据日志的类型,想日志对象中的数据添值
+// 比较关键的是日志所在事务的标号,该日志的标号,对数据库修改前后的变化
 static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
   lsn_t lsn = LogRec::next_lsn_++;
   auto log = std::make_shared<LogRec>(LogRecType::kInsert, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
@@ -92,6 +95,7 @@ static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_v
   return log;
 }
 
+// 开始也是一种日志
 static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
   lsn_t lsn = LogRec::next_lsn_++;
   return std::make_shared<LogRec>(LogRecType::kBegin, lsn, txn_id, LogRec::GetAndUpdatePrevLSN(txn_id, lsn));
--- src/include/recovery/recovery_manager.h
+++ src/include/recovery/recovery_manager.h
@@ -11,85 +11,94 @@ using KvDatabase = std::unordered_map<KeyType, ValType>;
 using ATT = std::unordered_map<txn_id_t, lsn_t>;
 
 struct CheckPoint {
-  lsn_t checkpoint_lsn_{INVALID_LSN};
-  ATT active_txns_{};
-  KvDatabase persist_data_{};
+  lsn_t checkpoint_lsn_{INVALID_LSN};	// 检查点对应的日志序列号
+  ATT active_txns_{}; // 映射表，表示在检查点时刻活跃事务的集合。键是事务ID，值是这个事务最后一个日志记录的LSN
+  KvDatabase persist_data_{}; // 映射表，表示在检查点时刻已经被持久化到数据库的数据。键是数据项的键，值是数据项的值
 
+	// 一个活跃事务添加到活跃事务集合
   inline void AddActiveTxn(txn_id_t txn_id, lsn_t last_lsn) { active_txns_[txn_id] = last_lsn; }
-
+	// 把一个数据项添加到持久化数据集合
   inline void AddData(KeyType key, ValType val) { persist_data_.emplace(std::move(key), val); }
 };
 
 class RecoveryManager {
  public:
   void Init(CheckPoint &last_checkpoint) {
+		// 检查点包含了已经持久化的数据,检查点所在的日志序列号,在检查点时刻还活跃的事务
     persist_lsn_ = last_checkpoint.checkpoint_lsn_;
+		// 使用std::move的原因是set,map类似的大型的数据结构,如果使用简单的=赋值,在内存拷贝的时候效率较低
+		// std::move通过直接将源对象的资源（通常是堆上的资源）“移动”到目标对象，避免了不必要的拷贝
     active_txns_ = std::move(last_checkpoint.active_txns_);
     data_ = std::move(last_checkpoint.persist_data_);
   }
 
+	// 用于恢复过程的重做阶段
+	// 由于幂等性,可以通过日志来将数据库系统从上一个检查点恢复到宕机前的状态
+	// 不是幂等性的语句,我们实验不考虑
   void RedoPhase() {
+		// 正常情况下,log_recs_如何获得:
+		// 在执行一个操作,比如insert,delete,commit,abort等,都会生成一条日志,
+		// 这个时候,就会像log_recs中插入一个新信息
+		// 正常数据库中都会有很多条日志,所以我想设计的时候应该按时将log_recs中在检查点之前的都删掉
     auto iter = log_recs_.begin();
-    // starts from checkpoint_lsn
+		// 在检查点之前的,即日志编号在persist_lsn_之前的,都认为正常
     for (; iter != log_recs_.end() && iter->first < persist_lsn_; ++iter) {
     }
+		// 从检查点开始,根据日志信息进行重做,本次实验重做仅仅实现了简单内存上的重做,并不和其他模块相关联
     for (; iter != log_recs_.end(); ++iter) {
       auto &log_rec = *(iter->second);
       active_txns_[log_rec.txn_id_] = log_rec.lsn_;
-      switch (log_rec.type_) {
-        case LogRecType::kInvalid:
-          break;
-        case LogRecType::kInsert:
-          data_[log_rec.ins_key_] = log_rec.ins_val_;
-          break;
-        case LogRecType::kDelete:
-          data_.erase(log_rec.del_key_);
-          break;
-        case LogRecType::kUpdate:
-          data_.erase(log_rec.old_key_);
-          data_[log_rec.new_key_] = log_rec.new_val_;
-          break;
-        case LogRecType::kBegin:
-          break;
-        case LogRecType::kCommit:
-          active_txns_.erase(log_rec.txn_id_);
-          break;
-        case LogRecType::kAbort:
-          Rollback(log_rec.txn_id_);
-          active_txns_.erase(log_rec.txn_id_);
-          break;
-      }
+			if (log_rec.type_ == LogRecType::kInvalid) {
+				continue;
+			} else if (log_rec.type_ == LogRecType::kInsert) {
+				data_[log_rec.ins_key_] = log_rec.ins_val_;
+			} else if (log_rec.type_ == LogRecType::kDelete) {
+				data_.erase(log_rec.del_key_);
+			} else if (log_rec.type_ == LogRecType::kUpdate) {
+				data_.erase(log_rec.old_key_);
+				data_[log_rec.new_key_] = log_rec.new_val_;
+			} else if (log_rec.type_ == LogRecType::kBegin) {
+				if (active_txns_.find(log_rec.txn_id_) == active_txns_.end()) {
+					active_txns_[log_rec.txn_id_] = log_rec.lsn_;
+				}
+			} else if (log_rec.type_ == LogRecType::kCommit) {
+				active_txns_.erase(log_rec.txn_id_);
+			} else if (log_rec.type_ == LogRecType::kAbort) {
+				Rollback(log_rec.txn_id_);
+			} else {
+				assert(false); //invalid area
+			}
     }
   }
 
   void UndoPhase() {
+		std::vector<txn_id_t> vec_txn;
     for (const auto &[txn_id, _] : active_txns_) {
-      Rollback(txn_id);
+      vec_txn.push_back(txn_id);
     }
-    active_txns_.clear();
+		for (const auto txn_id : vec_txn) {
+			Rollback(txn_id);
+		}
   }
 
+	// 对未完成的事务进行回滚
   void Rollback(txn_id_t txn_id) {
+		// 获得这一事务已经进行到的日志标号
     auto last_log_lsn = active_txns_[txn_id];
     while (last_log_lsn != INVALID_LSN) {
       auto log_rec = log_recs_[last_log_lsn];
-      if (log_rec == nullptr) break;
-      switch (log_rec->type_) {
-        case LogRecType::kInsert:
-          data_.erase(log_rec->ins_key_);
-          break;
-        case LogRecType::kDelete:
-          data_[log_rec->del_key_] = log_rec->del_val_;
-          break;
-        case LogRecType::kUpdate:
-          data_.erase(log_rec->new_key_);
-          data_[log_rec->old_key_] = log_rec->old_val_;
-          break;
-        default:
-          break;
-      }
+			// 根据日志类型进行撤销
+			if (log_rec->type_ == LogRecType::kInsert) {
+				data_.erase(log_rec->ins_key_);
+			} else if (log_rec->type_ == LogRecType::kDelete) {
+				data_[log_rec->del_key_] = log_rec->del_val_;
+			} else if (log_rec->type_ == LogRecType::kUpdate) {
+				data_.erase(log_rec->new_key_);
+				data_[log_rec->old_key_] = log_rec->old_val_;	
+			}
       last_log_lsn = log_rec->prev_lsn_;
     }
+		active_txns_.erase(txn_id);
   }
 
   // used for test only
